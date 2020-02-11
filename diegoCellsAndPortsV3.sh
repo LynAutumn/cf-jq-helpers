6bf23d07e9293c90c4cbe001ef2f7f1ed2d0ca80
LynAutumn <stefanie.herbst@gmx.de>
2020-02-11 14:11:54 +0100

    Formatted output

--- diegoCellsAndPortsV3.sh
+++ diegoCellsAndPortsV3.sh
@@ -7,7 +7,7 @@ while [[ "${next_url}" != "null" ]]; do
   started_apps=$(cf curl ${next_url} | jq -r '.resources[] | select(.state == "STARTED")'.guid)
   for app_guid in $started_apps; do
     echo "AppGuid: ${app_guid}:"
-    cf curl /v3/processes/${app_guid}/stats | jq -r -c '.resources[] | [.host, .instance_ports[].external, .instance_ports[].internal] | @tsv'
+    cf curl /v3/processes/${app_guid}/stats | jq -r -c '.resources[] | [ "host: ", .host, "port_external: ", .instance_ports[].external, "port_internal: ", .instance_ports[].internal] | @tsv'
   done
   next_url=$(cf curl ${next_url} | jq -r -c ".pagination.next.href" | sed 's/.*.com//;s/.*.cn//')
 done
--- findAndStopStackApps.sh
+++ findAndStopStackApps.sh
@@ -7,7 +7,7 @@ while [[ "${next_url}" != "null" ]]; do
   stack_app_guids=$(cf curl ${next_url} | jq -r '.resources[] | select(.lifecycle.data.stack == '\"$desiredStack\"')'.guid)
   for app_guid in $stack_app_guids; do
     echo
-    cf curl /v3/apps/$app_guid | jq -r '[.guid, .name, .state, .lifecycle.data.buildpacks[], .lifecycle.data.stack, .relationships.space.data.guid] | @tsv'
+    cf curl /v3/apps/$app_guid | jq -r '["AppGuid: ", .guid, "Name: ", .name, "State: ", .state, "Buildpack: ", .lifecycle.data.buildpacks[], "Stack: ", .lifecycle.data.stack, "SpaceGuid: ", .relationships.space.data.guid] | @tsv'
     read -p "Are you sure to stop above app (AppGuid, Name, State, Buildpacks, Stack, SpaceGuid)? [yY/any other char for no]: " -n 1 -r
     echo
 
